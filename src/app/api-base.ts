f51827f47388d39f7dc3c12f471caec7f16d8378
SURIYA <22lcs01@psr.edu.in>
2025-11-04 13:19:02 +0530

    approval pending update until

--- src/app/api-base.ts
+++ src/app/api-base.ts
@@ -1,7 +1,7 @@
 export class clienturl {
 
     static CURRENT_VERSION(): String {
-        return "v1.0.4"
+        return "v1.0.0"
     }
 
     static RELEASE_DATE(): String {
@@ -12,21 +12,26 @@ export class clienturl {
     static SERVER_URL(): String {
 
         return 'https://ds.iqtv.in:8080/iqserver/api/server/getserverdetails';
+        //  return 'http://192.168.70.100:8585/iqserver/api/server/getserverdetails';
     }
 
 
     static WEB_URL(): String {
 
         return "https://ds.iqtv.in";
+        // return "http://192.168.70.100";
+
     }
     static AUTH_URL(): String {
         
         return 'https://ds.iqtv.in:8080/iqworld/api/auth';
+        //  return 'http://192.168.70.100:8585/iqworld/api/auth';
     }
 
     static BASE_URL(): String {
 
         return 'https://ds.iqtv.in:8080/iqworld';
+        //  return 'http://192.168.70.100:8585/iqworld';
     }
 
 }
